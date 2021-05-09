6dc44624ca808c9e42a8a1983fe4a2c5a73b783f
Agustin Siles <agustinsiles10@gmail.com>
2021-05-09 18:09:39 -0300

    Removed unnecessary dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/home/home.component';
 import MovieDetails from './pages/movie-details/movie-details.component';
 
